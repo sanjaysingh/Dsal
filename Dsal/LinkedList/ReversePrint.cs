20d46db34541c2bcfc07e2dff039ece4d4495a25
Sanjay Singh <itssanjay@gmail.com>
2016-03-13 17:12:09 -0400

    repverprint linked list

---
+++ Dsal/LinkedList/ReversePrint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsal.LinkedList
+{
+    public static class ReversePrintExtension 
+    {
+        public static List<int> ReversePrint(this DsalLinkedList linkedList)
+        {
+            return ReversePrint(linkedList.Head);
+        }
+
+        private static List<int> ReversePrint(LinkedListNode node)
+        {
+            if (node == null)
+            {
+                return new List<int>();
+            }
+            var printedList = ReversePrint(node.Next);
+            printedList.Add(node.Data);
+            return printedList;
+        }
+    }
+}
---
+++ DsalTest/LinkedList/ReversePrintSpec.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dsal.LinkedList;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace DsalTest.LinkedList
+{
+    [TestClass]
+    public class ReversePrintSpec
+    {
+        [TestMethod]
+        public void ReversePrint5ItemsLinkedListShouldSucceed()
+        {
+            DsalLinkedList linkedList = new DsalLinkedList();
+
+            linkedList
+                .AddItems(new int[] { 5, 7, 10, 3, 9 })
+                .ReversePrint()
+                .Should().BeEquivalentTo(9, 3, 10, 7, 5);
+        }
+        [TestMethod]
+        public void ReversePrintSingleItemLinkedListShouldSucceed()
+        {
+            DsalLinkedList linkedList = new DsalLinkedList();
+
+            linkedList
+                .AddItems(new int[] { 5 })
+                .ReversePrint()
+                .Should().BeEquivalentTo(5);
+        }
+    }
+}
