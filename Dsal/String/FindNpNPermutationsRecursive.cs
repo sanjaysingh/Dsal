decb4d56b111d906873fe3c6c215578a601fe430
Sanjay Singh <itssanjay@gmail.com>
2016-04-08 20:43:57 -0400

    fixing build issue

---
+++ Dsal/String/FindNpNPermutationsRecursive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsal.String
+{
+    public static class FindNpNPermutationsRecursiveExtensions
+    {
+        public static IEnumerable<string> FindNpNPermutationsRecursive(this string data)
+        {
+            foreach(string permutation in FindNpNPermutationsRecursive(data, string.Empty))
+            {
+                yield return permutation;
+            }
+        }
+
+        private static IEnumerable<string> FindNpNPermutationsRecursive(string substring, string remaining)
+        {
+            if (string.IsNullOrEmpty(substring)) yield return remaining;
+            for (int i = 0; i < substring.Length; i++)
+            {
+                foreach (var permutation in FindNpNPermutationsRecursive(substring.Remove(i, 1), remaining + substring[i]))
+                {
+                    yield return permutation;
+                }
+            }
+        }
+    }
+}
---
+++ Dsal/String/IsPalindrome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsal.String
+{
+    public static class IsPalindromeExtension
+    {
+        public static bool IsPalindrome(this string data)
+        {
+            int start = 0, end = data.Length - 1;
+            
+            while (start < end && char.ToLower(data[start]) == char.ToLower(data[end]))
+            {
+                start++;
+                end--;
+            }
+
+            return start >= end;
+        }
+    }
+}
