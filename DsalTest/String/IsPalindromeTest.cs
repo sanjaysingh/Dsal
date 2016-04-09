6fee60865fe5eecc287089b9cfcfe296930f3f42
Sanjay Singh <itssanjay@gmail.com>
2016-04-08 20:37:17 -0400

    string algos

---
+++ DsalTest/String/IsPalindromeTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Dsal.String;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace DsalTest.String
+{
+    [TestClass]
+    public class IsPalindromeTest
+    {
+        [TestMethod]
+        public void IsPalindromeOnOddNumberOfCharsForPalindromeShouldWork()
+        {
+            "civic"
+                .IsPalindrome()
+                .Should()
+                .BeTrue();
+        }
+
+        [TestMethod]
+        public void IsPalindromeOnOddNumberOfCharsForNonPalindromeShouldWork()
+        {
+            "Perls"
+                .IsPalindrome()
+                .Should()
+                .BeFalse();
+        }
+
+        [TestMethod]
+        public void IsPalindromeOnEvenNumberOfCharsForPalindromeShouldWork()
+        {
+            "Hannah"
+                .IsPalindrome()
+                .Should()
+                .BeTrue();
+        }
+
+        [TestMethod]
+        public void IsPalindromeOnEvenNumberOfCharsForNonPalindromeShouldWork()
+        {
+            "Handenah"
+                .IsPalindrome()
+                .Should()
+                .BeFalse();
+        }
+
+        [TestMethod]
+        public void IsPalindromeOnSingleCharacterShouldWork()
+        {
+            "A"
+                .IsPalindrome()
+                .Should()
+                .BeTrue();
+        }
+
+        [TestMethod]
+        public void IsPalindromeOnEmptyStringShouldWork()
+        {
+            ""
+                .IsPalindrome()
+                .Should()
+                .BeTrue();
+        }
+    }
+}
