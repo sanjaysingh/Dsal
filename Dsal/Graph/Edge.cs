4176814f7957a9990cf9dc6e16ee808c936f1357
Sanjay Singh <itssanjay@gmail.com>
2018-03-07 22:52:49 -0500

    uprading to 4.6.1

---
+++ Dsal/Graph/Edge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsal.Graph
+{
+    public class Edge
+    {
+        public Edge(Vertex from, Vertex to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public readonly Vertex From;
+        public readonly Vertex To;
+    }
+}
---
+++ Dsal/Graph/Vertex.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Dsal.Graph
+{
+    public class Vertex
+    {
+        private List<Edge> neighbors = new List<Edge>();
+
+        public Vertex(string id) => this.Id = id;
+
+        public readonly string Id;
+
+        public IEnumerable<Edge> Neighbors
+        {
+            get
+            {
+                return neighbors;
+            }
+        }
+    }
+}
